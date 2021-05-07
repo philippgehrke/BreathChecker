fa7c2b97a2cdde46dd03a4d4c274d39eca3de524
philippgehrke <37164282+philippgehrke@users.noreply.github.com>
2021-05-07 12:26:42 +0200

    Initial Commit

---
+++ Breath_Checker/Breath_CheckerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Breath_CheckerApp.swift
+//  Breath_Checker
+//
+//  Created by Jan Philipp Gehrke on 5/7/21.
+//
+
+import SwiftUI
+
+@main
+struct Breath_CheckerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Breath_Checker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Breath_Checker
+//
+//  Created by Jan Philipp Gehrke on 5/7/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
